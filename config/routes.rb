e8b28f3c050f1c53c31ae7a9d1cfac74c3307d2a
eric9208 <eric@jane.app>
2023-05-24 22:01:19 -0700

    add ssl and Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
